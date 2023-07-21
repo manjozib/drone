a37aab7a00be473a42261e3561f0affd11014992
manjozib <manjozib@gmail.com>
2023-07-21 18:45:18 +0200

    model of Drone

---
+++ src/main/java/com/example/drone/model/Drone.java
@@ -0,0 +1,9 @@
+package com.example.drone.model;
+
+public class Drone {
+    private String serialNumber;
+    private String model;
+    private String weightLimit;
+    private String batteryCapacity;
+    private String state;
+}
