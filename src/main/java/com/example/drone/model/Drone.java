1bbf6c03b8003d243ecd30d3582f7977c257ed94
manjozib <manjozib@gmail.com>
2023-07-21 18:47:59 +0200

    adding getters, setters, construcrtors and set the class as an entity

--- src/main/java/com/example/drone/model/Drone.java
+++ src/main/java/com/example/drone/model/Drone.java
@@ -1,5 +1,18 @@
 package com.example.drone.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "drone")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Drone {
     private String serialNumber;
     private String model;
