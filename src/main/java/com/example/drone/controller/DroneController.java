8102b523a7ad7e81e00f39ef5f91850722ab723c
manjozib <manjozib@gmail.com>
2023-07-21 19:13:06 +0200

    controller for drone

---
+++ src/main/java/com/example/drone/controller/DroneController.java
@@ -0,0 +1,23 @@
+package com.example.drone.controller;
+
+
+import com.example.drone.model.Drone;
+import com.example.drone.service.DroneServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/drone")
+public class DroneController {
+
+    @Autowired
+    DroneServiceImpl droneService;
+
+    @RequestMapping(value ="/register", method = RequestMethod.POST)
+    public String registerDrone(@RequestBody Drone drone) {
+        return droneService.registeringDrone(drone);
+    }
+}
