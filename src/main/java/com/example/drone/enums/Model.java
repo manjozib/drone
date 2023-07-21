dd65e237ea5767ac1e34619543f8a991fb0250de
manjozib <manjozib@gmail.com>
2023-07-21 22:45:58 +0200

    Enums to allow string json type

--- src/main/java/com/example/drone/enums/Model.java
+++ src/main/java/com/example/drone/enums/Model.java
@@ -1,5 +1,29 @@
 package com.example.drone.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.stream.Stream;
+
 public enum Model {
-    Lightweight, Middleweight, Cruiserweight, Heavyweight
+    Lightweight("Lightweight"),
+    Middleweight("Middleweight"),
+    Cruiserweight("Cruiserweight"),
+    Heavyweight("Heavyweight");
+
+    private String model;
+
+    private Model(String model) {
+        this.model=model;
+    }
+
+    @JsonCreator
+    public static Model decode(final String model) {
+        return Stream.of(Model.values()).filter(targetEnum -> targetEnum.model.equals(model)).findFirst().orElse(null);
+    }
+
+    @JsonValue
+    public String getModel() {
+        return model;
+    }
 }
--- src/main/java/com/example/drone/enums/State.java
+++ src/main/java/com/example/drone/enums/State.java
@@ -1,5 +1,31 @@
 package com.example.drone.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.stream.Stream;
+
 public enum State {
-    IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+    IDLE("IDLE"),
+    LOADING("LOADING"),
+    LOADED("LOADED"),
+    DELIVERING("DELIVERING"),
+    DELIVERED("DELIVERED"),
+    RETURNING("RETURNING");
+
+    private String state;
+
+    private State(String state) {
+        this.state=state;
+    }
+
+    @JsonCreator
+    public static State decode(final String state) {
+        return Stream.of(State.values()).filter(targetEnum -> targetEnum.state.equals(state)).findFirst().orElse(null);
+    }
+
+    @JsonValue
+    public String getState() {
+        return state;
+    }
 }
