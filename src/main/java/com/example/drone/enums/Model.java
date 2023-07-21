9fb7113e80c0c0e6445ea9ce821e6824e89d199d
manjozib <manjozib@gmail.com>
2023-07-21 18:52:39 +0200

    enums for state and model of drone

---
+++ src/main/java/com/example/drone/enums/Model.java
@@ -0,0 +1,5 @@
+package com.example.drone.enums;
+
+public enum Model {
+    Lightweight, Middleweight, Cruiserweight, Heavyweight
+}
---
+++ src/main/java/com/example/drone/enums/State.java
@@ -0,0 +1,5 @@
+package com.example.drone.enums;
+
+public enum State {
+    IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+}
